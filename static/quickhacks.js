4e39bc168b6078ae64f0538ad4d78c97827f5842
Martin Gergov <martingergov1@gmail.com>
2017-11-08 22:50:37 +0100

    new article test.

---
+++ static/quickhacks.js
@@ -0,0 +1,10 @@
+$.ajax({
+    url: 'https://raw.githubusercontent.com/marto1/my-ideas-list/master/hacks',
+    dataType: 'text',
+    success: function(data) 
+    {
+        // var text = data.split('\n');
+	// text.forEach(function(line){
+	//     console.log(line);
+	// });
+    }});
