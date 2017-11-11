c7072502fafb2ded04c37673fbd19fd2b8ed0d08
Martin Gergov <martingergov1@gmail.com>
2017-11-11 20:56:48 +0100

    added block quoting style and actual descriptions for quickhacks article

--- static/quickhack.js
+++ static/quickhack.js
@@ -1,3 +1,8 @@
+function include_description(element, description) {
+    element.html(element.html() +
+		"<p class=\"quotebash\">" + description + "</p>");
+}
+
 $.ajax({
     url: 'https://raw.githubusercontent.com/marto1/my-ideas-list/master/hacks',
     dataType: 'text',
@@ -5,13 +10,25 @@ $.ajax({
     {
 	var hacklist = $("#hacklist");
         var text = data.split('\n');
+	var description = "";
 	var v;
 	text.forEach(function(line){
+	    var lastel;
 	    if (line.startsWith("* ")){
+		if (description != "") {
+		    include_description(hacklist.children().last(),
+					description)
+		}
 		v = "<li>" + line.slice(2) + "</li>"
 		hacklist.append(v);
+		description = "";
 	    } else {
-		
+		description += line;
 	    }
 	});
+	//last element
+	if (description != "") {
+	    include_description(hacklist.children().last(),
+				description)
+	}
     }});
